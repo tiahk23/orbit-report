bad1a922e47c94b306e7a5c35d03b6f5b69e6b5e
Tiah Keefe <tetered12@gmail.com>
2021-07-02 11:44:47 -0500

    fixed a major issue in satellite.ts and then made sure my table was producing the correct output.

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -6,7 +6,11 @@ export class Satellite {
     launchDate: string;
 
     constructor(name:string, type:string, launchDate:string, orbitType:string, operational:boolean){
-        
+        this.name = name;
+        this.type = type;
+        this.launchDate = launchDate;
+        this.orbitType = orbitType;
+        this.operational = operational;
         
     }
 }
