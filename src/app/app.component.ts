47c4293d453b3daefb29e029d8fe5652362e1c68
Tiah Keefe <tetered12@gmail.com>
2021-07-04 10:29:27 -0500

    finished adding search element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,10 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -23,10 +25,22 @@ export class AppComponent {
             this.sourceList.push(newSatellite);
             
           }
-          
+          this.displayList = this.sourceList.slice(0);
         }.bind(this));
     }.bind(this));
  
+    
  }
   
+    search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+        }
+    }
+    this.displayList = matchingSatellites;
+    }
 }
